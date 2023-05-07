f4b48fe4c2014ac9ea271d1a1cb5567dfc7038fd
Victor Nikolenko <victor1982nik@gmail.com>
2023-05-07 22:49:45 +0300

    add styled components
    installed 5.3.9 version - 6.. version couldn't install

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-//import { theme } from "./theme";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//console.log(ThemeProvider);
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
-    <App />
-    {/* </ThemeProvider> */}
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
